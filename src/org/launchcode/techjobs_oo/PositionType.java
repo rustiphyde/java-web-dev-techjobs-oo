5059dfe41f7b75e83c83a8039a766020355cbeda
rustiphyde <rustiphyde@gmail.com>
2019-11-25 00:04:05 -0600

    DRY out the code to use inheritance from a base class

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -2,51 +2,9 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
+public class PositionType extends JobField{
 
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
-
-    public PositionType(String value) {
-        this();
-        this.value = value;
-    }
-
-    // Add a custom toString() method that returns the data stored in 'value'.
-    public String toString() { return value; }
-
-    // Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PositionType)) return false;
-        PositionType that = (PositionType) o;
-        return getId() == that.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+    public PositionType(String aValue){
+        super(aValue);
     }
 }
